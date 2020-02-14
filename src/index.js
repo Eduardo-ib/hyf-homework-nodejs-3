0ff5835cf904e6f2133243c4a6f471f0f5b26aaa
Eduardo-ib <gibarra@unitedvirtualities>
2020-02-14 12:48:41 -0700

    prueba

--- src/index.js
+++ src/index.js
@@ -2,51 +2,29 @@ const express = require('express');
 const request = require('request');
 const app = express();
 
-const bodyParser = require('body-parser');
-app.use(bodyParser.json());
-
-var listUsers =[];
+var listaUsers =[];
 
 app.get('/',function(req, res) {
     res.send('Hello World!');
 });
 
 app.post('/user',function(req, res){
-    listUsers.push(req.body);
+    listaUsers.push(req.body);
         res.status(200).json({
-            listUsers
+            listaUsers
     });
-    listUsers = [
-        {id:0}
-    ]
-    //console.log(listUsers);
+    listaUsers = [{id:0}]  
 });
 app.get('/users', function(req, res){
-    res.json(listUsers);
-    //console.log(listUsers);
+    res.json(listaUsers);
 });
 
 app.get('/user/:id', function(req, res){
-    const user = listUsers.find(us => us.id == req.params.id);
+    const user = listaUsers.find(us => us.id == req.params.id);
     res.json({
         id: 0
     });
 });
-
-/*app.delete('/user/:id',function(req, res){
-    const user = listUsers.find(us => us.id == req.params.id);
-    if(user){
-        res.status(202).json({
-            message: "Si existe el usuario",
-            user
-        });
-        listUsers.pop();
-    }else{
-        res.status(204).json({
-            message: "No existe el usuario"
-        });
-    }
-});*/
 
 app.listen(3000, function() {
     console.log("server is running");
