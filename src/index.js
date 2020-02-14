e09be59d21d77bc9331c8d6abc63017cacdea825
Eduardo-ib <gibarra@unitedvirtualities>
2020-02-14 11:32:35 -0700

    quinta correccion

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-var users=[];
+//var users=[];
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
@@ -19,22 +19,6 @@ app.get('/users', function(req,res){
   res.send(users)
 })
 
-app.delete('/user/:id', function (req, res) {
-  const user = users.find(us => us.id == req.params.id);
-  if (user){
-    res.status(202).json({
-      success: true,
-      message: 'Si existe el usuario',
-      user
-    })
-    users.pop();
-  } else {
-    res.status(204).json({
-         success: false,
-         message: 'password and username do not match'
-    })
-  }
-});
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
