8571b92c409e0ebe257948da107eb03b1843dbcd
Eduardo-ib <gibarra@unitedvirtualities>
2020-02-14 11:23:08 -0700

    cuarta correcion

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 var express = require('express');
 var app = express();
 
+var users=[];
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+pp.post('/user', function(req,res){
+  users.push(req.body)
+  res.status(200).json({
+    users
+  })
+  users = [{id:0}] 
+})
 
+app.get('/users', function(req,res){
+  res.send(users)
+})
+
+app.delete('/user/:id', function (req, res) {
+  const user = users.find(us => us.id == req.params.id);
+  if (user){
+    res.status(202).json({
+      success: true,
+      message: 'Si existe el usuario',
+      user
+    })
+    users.pop();
+  } else {
+    res.status(204).json({
+         success: false,
+         message: 'password and username do not match'
+    })
+  }
+});
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
