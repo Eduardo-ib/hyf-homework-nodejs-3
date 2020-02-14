309fba864418b63ae377ecd3141ba005d1daa118
Eduardo-ib <gibarra@unitedvirtualities>
2020-02-14 12:32:17 -0700

    intento

--- src/index.js
+++ src/index.js
@@ -1,22 +1,53 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const request = require('request');
+const app = express();
+const bodyParser = require('body-parser');
 
+app.use(bodyParser.json());
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+var listUsers =[];
+
+app.get('/',function(req, res) {
+    res.send('Hello World!');
 });
 
-app.get('/users', function(req,res){
-  res.send(users)
-})
+app.post('/user',function(req, res){
+    listUsers.push(req.body);
+        res.status(200).json({
+            listUsers
+    });
+    listUsers = [
+        {id:0}
+    ]
+    //console.log(listUsers);
+});
+app.get('/users', function(req, res){
+    res.json(listUsers);
+    //console.log(listUsers);
+});
 
-app.get('/user/:id',function(req,res){
-  const user = users.find(us => us.id == req.params.id);
+app.get('/user/:id', function(req, res){
+    const user = listUsers.find(us => us.id == req.params.id);
     res.json({
         id: 0
     });
-})
+});
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
-  });
+app.delete('/user/:id',function(req, res){
+    const user = listUsers.find(us => us.id == req.params.id);
+    if(user){
+        res.status(202).json({
+            message: "Si existe el usuario",
+            user
+        });
+        listUsers.pop();
+    }else{
+        res.status(204).json({
+            message: "No existe el usuario"
+        });
+    }
+});
+
+app.listen(3000, function() {
+    console.log("server is running");
+});
