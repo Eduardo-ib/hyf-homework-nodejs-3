7d15e59fdc241eca12da75125e6d19321209c96f
Eduardo-ib <gibarra@unitedvirtualities>
2020-02-18 16:05:08 -0700

    step-3

--- src/index.js
+++ src/index.js
@@ -10,6 +10,24 @@ app.get('/users', function (req, res) {
     res.send(listaUser);
   });
 
+  app.post('/user', function (req, res) {
+    listaUser.push(req.body);
+    res.status(200).json({
+        listaUser
+    });
+    listaUser = [
+        {id:0}
+    ]
+  });
+
+  app.get('/user/:id', function(req, res){
+    const user = listaUser.find(us => us.id == req.params.id);
+    res.json({
+        id: 0
+    });
+});
+
+
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
   });
